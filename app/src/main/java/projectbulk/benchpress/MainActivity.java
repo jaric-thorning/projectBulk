158e2886d276877abdf38200187a9c2d974f1ab7
Jaric Thorning <jaric.thorning@gmail.com>
2016-05-10 01:24:00 +1000

    Added settings activity, changed icon

--- app/src/main/java/projectbulk/benchpress/MainActivity.java
+++ app/src/main/java/projectbulk/benchpress/MainActivity.java
@@ -1,5 +1,6 @@
 package projectbulk.benchpress;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -87,8 +88,11 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
-
+            Intent myIntent = new Intent(MainActivity.this, SettingsActivity.class);
+            //myIntent.putExtra("key", value); //Optional parameters
+            MainActivity.this.startActivity(myIntent);
         } else if (id == R.id.nav_share) {
+
 
         } else if (id == R.id.nav_send) {
 
