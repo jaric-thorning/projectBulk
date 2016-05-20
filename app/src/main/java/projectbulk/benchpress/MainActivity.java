42db8105baca0cdd89dc8da727ac58b0bdb19516
Jaric Thorning <jaric.thorning@gmail.com>
2016-05-20 19:45:11 +1000

    Changed sidebar titles and icons

--- app/src/main/java/projectbulk/benchpress/MainActivity.java
+++ app/src/main/java/projectbulk/benchpress/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -81,11 +82,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_start_workout) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_logs) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_progress) {
 
         } else if (id == R.id.nav_manage) {
             Intent myIntent = new Intent(MainActivity.this, SettingsActivity.class);
